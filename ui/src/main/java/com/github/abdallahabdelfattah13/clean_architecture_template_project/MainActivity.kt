2dcb5c25a38f8b4866a93b9ab24593ed30e8b15c
AbdAllah <AbdAllah.abdelfattah@thed.io>
2019-05-27 13:19:41 +0200

    init commit.

---
+++ ui/src/main/java/com/github/abdallahabdelfattah13/clean_architecture_template_project/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.abdallahabdelfattah13.clean_architecture_template_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
